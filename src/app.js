67122f124bbe031b5bc6834408d73f4d07f8ef63
nguyenvo200190 <42613841+nguyenvo200190@users.noreply.github.com>
2020-04-06 22:43:06 -0400

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
 const app = express()
-
+const port = process.env.PORT || 3000
 //Define paths for Espress config
 const homePath =path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -55,6 +55,6 @@ app.get('*',(req,res)=>{
     })
 
 })
-app.listen(3000, ()=>{
-    console.log("Sever is up on port 3000.")
+app.listen(port, ()=>{
+    console.log("Sever is up on port" +port)
 })
