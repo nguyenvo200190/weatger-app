6cfb0cbb320eb50246e5a3543e8445ac11cd544e
nguyenvo200190 <42613841+nguyenvo200190@users.noreply.github.com>
2020-04-06 20:54:41 -0400

    Init commit

---
+++ src/app.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const app = express()
+
+//Define paths for Espress config
+const homePath =path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath =path.join(__dirname, '../templates/partials')
+// Setup handlebars engin and views location
+app.set('view engine','hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+//Setup static direct
+app.use(express.static(homePath))
+
+
+app.get('',(req,res)=>{
+    res.render('HOMEPAGE',{
+        title: "Weather App",
+        name:'Nguyen author'
+    })
+})
+app.get('/help',(req,res)=>{
+    res.send({
+        name: 'Nguyen',
+        age:27
+    })
+
+})
+app.get('/homepage',(req,res)=>{
+    res.render('HOMEPAGE')
+
+})
+app.get('/weather',(req,res)=>{
+    res.send("page weather ")
+
+})
+//app.com
+//app.com/help
+//app.com/about
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title:"404",
+        name:"nguyen",
+        errorMessage:"Help article not found"
+    })
+
+})
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title:'404',
+        name:"nguyen",
+        errorMessage:'Page Not Found'
+    })
+
+})
+app.listen(3000, ()=>{
+    console.log("Sever is up on port 3000.")
+})
